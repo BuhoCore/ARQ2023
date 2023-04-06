daecebe60b0e5b8123584b39e3c116b0fc1dbc5f
BuhoCore <grexcold@hotmail.com>
2023-04-06 11:37:44 -0600

    act Opcional Actualizacion

--- Actividad 9 Opcional/clase-12-angular-inventario-uia-examen-master/src/app/new-solicitud-entregada/new-solicitud-entregada.component.ts
+++ Actividad 9 Opcional/clase-12-angular-inventario-uia-examen-master/src/app/new-solicitud-entregada/new-solicitud-entregada.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { DatosService } from '../datos.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalSubpartidasComponent } from '../modal-subpartidas/modal-subpartidas.component';
+import { IPartida } from '../IPartida';
 
 @Component({
   selector: 'app-new-solicitud-entregada',
@@ -7,4 +13,57 @@ import { Component } from '@angular/core';
 })
 export class NewSolicitudEntregadaComponent {
 
+  partidas: IPartida[] = [];
+  pageActual:number=1;
+  urlaccion:string = 'assets/icon-tabla/';
+  //Material table columns
+  displayedColumns: string[] = ['id', 'idPartida', 'idSubpartida', 'idCategoria', 'idProveedor', 'nameCategoria', 'nameSubpartida'];
+  //Table Data Source
+  dataSource: any;
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+  //Dynamic Data Variable
+  data: any;
+  public totalRows = 0;
+
+  selectedPartida?: IPartida;
+ 
+   constructor(public datosPartida:DatosService,  public modal: MatDialog) 
+   {     
+
+   }
+                
+   ngOnInit(): void {
+                
+     this.datosPartida.getPartidas().subscribe((data: IPartida[])=>{
+       console.log(data);
+       this.partidas = data;
+     })
+     
+ 
+   }
+
+  solicitudMaterial(app: string,  id: any) {
+    console.log("id"+id);
+    console.log("app"+app);
+    console.log(id,app)
+    
+  }
+
+  openDialogNew(id:string) {
+    console.log("id:"+id);
+    this.datosPartida.setIdPartida(id);
+    const dialogRef = this.modal.open(ModalSubpartidasComponent, {
+      autoFocus: false,
+      maxHeight: '90vh' //you can adjust the value as per your view
+  });
+  
+    dialogRef.afterClosed().subscribe((result: any) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  
+  }
+
+
 }
+       
+  
